78129d2cc473ce440a18b786ed9873b5f7dc7b3e
Sergio <sergio@tinkerista.com>
2014-07-28 17:23:43 +0200

    añadadido mecanismo de escenas

---
+++ espectros/src/fantasmas.h
@@ -0,0 +1,84 @@
+#pragma once
+
+#include "ofxUI.h"
+#include "ofMain.h"
+#include "cheapComm.h"
+#include "ofxSyphon.h"
+#include "ofxCv.h"
+#include "mosaicPlayer.h"
+#include "mosaicRecorder.h"
+#include "ofxTuio.h"
+#include "moveAndRecord.h"
+#include "ofxImageSequence.h"
+#include <ofMath.h>
+#include "ofxScene.h"
+
+#define MAX_SEQUENCES 3
+class fantasmas : public ofxScene{
+
+	public:
+		void setup();
+		void update(float );
+		void draw();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+    
+    	ofxSyphonClient *mSyphonClient;
+        ofxSyphonClient *mSyphonClient2;
+    
+        void setSyphonClients( ofxSyphonClient *mSyphonClient_,ofxSyphonClient *mSyphonClient2_ ){
+            cout << "settings syphon client" <<endl;
+            mSyphonClient=mSyphonClient_;
+            mSyphonClient2=mSyphonClient2_;
+        }
+    
+	    cheapComm myOSCcomm;
+    //32 bits red, 32 bits green, 32 bits blue, from 0 to 1 in 'infinite' steps
+    	ofFbo fbo; // with alpha
+        	ofFbo fboGeneral; // with alpha
+    ofPixels cameraPixels;
+    ofImage cameraImg;
+    
+    ofxTuioClient *tuioclient;
+    void setTuioClient(ofxTuioClient *mtuioclient){
+        tuioclient=mtuioclient;
+    }
+    
+/*    void	tuioAdded(ofxTuioCursor & tuioCursor);
+	void	tuioRemoved(ofxTuioCursor & tuioCursor);
+	void	tuioUpdated(ofxTuioCursor & tuioCursor);
+    */
+    moveAndRecord moveandrecord;
+    mosaicRecorder mRecorder;
+    ofPixels remoteBlobImgPxl;
+    ofImage testImg;
+    ofImage feedImg;
+    ofxUICanvas *gui2;
+    std::map<string, int> appStatuses;
+     void setupStatus();
+    enum mode{CAPTURE, MOSAIC};
+    
+    
+    void setupSequences();
+    void setupSequence(int seq);
+    vector<ofxImageSequence> sequences;
+    void showDebug();
+    int RecordingBlobId;
+    
+    vector<ofPoint> points[MAX_SEQUENCES];
+    
+    void onRecordingFinished(int &num);
+    void playImage(int i);
+    int playImgIndex[MAX_SEQUENCES];
+    void gui2Event(ofxUIEventArgs &e);
+    int currentImg=0;
+    int maxFrame=50;
+};
--- espectros/src/testApp.cpp
+++ espectros/src/testApp.cpp
@@ -1,347 +1,96 @@
 #include "testApp.h"
-
-//--------------------------------------------------------------
-
-using namespace ofxCv;
-using namespace cv;
+//#include "consts.h"
 
 void testApp::setup(){
-    ofSetFrameRate(20);
-        setupStatus();
-    myOSCcomm.setup();
-    //Syphon client
-    mSyphonClient.setup();    
+    
+	ofSetFrameRate(60);
+	ofEnableAlphaBlending();
+	ofSetVerticalSync(true);
+	ofBackground(32, 32, 32);
+	ofEnableSmoothing();
+    
+    tuioClient.start(3333);
+    ofAddListener(tuioClient.cursorAdded,this,   &testApp::tuioAdded);
+	ofAddListener(tuioClient.cursorRemoved,this, &testApp::tuioRemoved);
+	ofAddListener(tuioClient.cursorUpdated,this, &testApp::tuioUpdated);
+	///////////////////////////////////////
+	
+	sceneManager = ofxSceneManager::instance();
+
+    mSyphonClient.setup();
     mSyphonClient.set("CameraOutput","");
     
     mSyphonClient2.setup();
     mSyphonClient2.set("onlyBlobs","");
-
-    fbo.allocate(640, 480, GL_RGBA);
-    //fboGeneral.allocate(640, 480, GL_RGB);
-    remoteBlobImgPxl.allocate(640,480,OF_PIXELS_RGBA);
-   // cameraImg.allocate(640, 480,OF_IMAGE_COLOR );
-    //mPlayer.setup();
-    tuioclient.start();
-
-    moveandrecord.setup();
-        //feedImg.allocate(640,480,OF_IMAGE_COLOR_ALPHA);
-    testImg.loadImage("test.jpg");
-    mRecorder.setup();
-        setupSequences();
     
+//	sceneManager->addScene( new electromagnetica(), SCENE_2);
+    fantasmas *mfantasmas=new fantasmas();
+    mfantasmas->setTuioClient(&tuioClient);
+    mfantasmas->setSyphonClients(&mSyphonClient, &mSyphonClient2);
     
-        ofAddListener(finishedRecordingEvent,this, &testApp::onRecordingFinished);
-    
-     gui2 = new ofxUICanvas(1038,583, 295,285);
-    vector<string> vnames; vnames.push_back("0"); vnames.push_back("1"); vnames.push_back("2");
-    gui2->addLabel("VERTICAL RADIO", OFX_UI_FONT_MEDIUM);
-    ofxUIRadio *radio = gui2->addRadio("VR", vnames, OFX_UI_ORIENTATION_HORIZONTAL);
-    radio->activateToggle("0");
-    ofAddListener(gui2->newGUIEvent,this,&testApp::gui2Event);
-    gui2->addIntSlider("maxFrame", 0, 49, &maxFrame);
-    gui2->addLabel("GRABACION", OFX_UI_FONT_MEDIUM);
-    gui2->addToggle("grabacion", false);
-}
-
-void testApp::setupSequences(){
-
-    ofxXmlSettings settings;
-    for(int i=0; i<MAX_SEQUENCES;i++){
-        sequences.push_back(ofxImageSequence());
-        //points[i]= vector<ofPoint>();
-    }
-    int ii=0;
-    for (std::vector<ofxImageSequence>::iterator it = sequences.begin() ;
-         it != sequences.end(); ++it){
-    	//it->loadSequence(ofToDataPath("recordings/video_"+ofToString(ii)+"/frame" , "png", 0, 75, 4);
-        it->loadSequence(ofToDataPath("recordings/video_"+ofToString(ii)));
-        it->setFrameRate(25);
-        it->preloadAllFrames();
-        
-    //LOAD POINTS
-        vector<ofPoint> mpoints;
-        if(settings.loadFile("recordings/positions"+ofToString(ii)+".xml")){
-            settings.pushTag("positions");
-            int numberOfSavedPoints = settings.getNumTags("position");
-            for(int i = 0; i < numberOfSavedPoints; i++){
-                settings.pushTag("position", i);
-                ofPoint p;
-                p.x = settings.getValue("X", 0);
-                p.y = settings.getValue("Y", 0);
-                mpoints.push_back(p);
-                settings.popTag();
-            }
-            settings.popTag(); //pop position
-            points[ii]=mpoints;
-        }
-        else{
-            ofLogError("Position file did not load!");
-            ofLogError("recordings/positions"+ofToString(ii)+".xml");
-        }
-        ii++;
-    }
+	sceneManager->addScene( mfantasmas, SCENE_1);
+	
+	sceneManager->setDrawDebug(true);
+	sceneManager->setCurtainDropTime(1.0);
+	sceneManager->setCurtainStayTime(0.0);
+	sceneManager->setCurtainRiseTime(1.0);
+	sceneManager->setOverlapUpdate(true);
+    //sceneManager->goToScene(SCENE_2);
     
 }
 
-void testApp::setupSequence(int seq){
-        sequences[seq].loadSequence(ofToDataPath("recordings/video_"+ofToString(seq)));
-        sequences[seq].setFrameRate(25);
-        sequences[seq].preloadAllFrames();
-        ofxXmlSettings settings;
-        vector<ofPoint> mpoints;
-        if(settings.loadFile("recordings/positions"+ofToString(seq)+".xml")){
-            settings.pushTag("positions");
-            int numberOfSavedPoints = settings.getNumTags("position");
-            for(int i = 0; i < numberOfSavedPoints; i++){
-                settings.pushTag("position", i);
-                ofPoint p;
-                p.x = settings.getValue("X", 0);
-                p.y = settings.getValue("Y", 0);
-                mpoints.push_back(p);
-                settings.popTag();
-            }
-            settings.popTag(); //pop position
-            points[seq]=mpoints;
-        }
-        else{
-            ofLogError("Position file did not load!");
-            ofLogError("recordings/positions"+ofToString(seq)+".xml");
-        }
-}
-
-void testApp::setupStatus(){
-    appStatuses["syphonEnabled"]=true;
-    appStatuses["mode"]=MOSAIC;
-    appStatuses["debug"]=true;
-    
-}
-
-//--------------------------------------------------------------
 void testApp::update(){
-  //  mPlayer.update();
-    tuioclient.getMessage();
-
-    if(appStatuses["mode"]==CAPTURE){
-        ofxTuioCursor* blobToRecord=moveandrecord.detectBlobinMouse(tuioclient.getTuioCursors(),mouseX,mouseY);
-        if(blobToRecord!=NULL && mRecorder.isRecording==false){
-        //if(moveandrecord.detectMouseinSquare(mouseX, mouseY)){
-            mRecorder.current_video=moveandrecord.currentRect;
-            cout<< "width" << blobToRecord->width<< "height"<< blobToRecord->height;
-            mRecorder.start(2000, 25, remoteBlobImgPxl,
-                            blobToRecord->width*640,blobToRecord->height*320,
-                           blobToRecord->getX()*640,blobToRecord->getY()*320
-                            );
-            RecordingBlobId=blobToRecord->getSessionId();
-        }
-        if( mRecorder.isRecording ){
-            bool blobStillExists=false;
-            list<ofxTuioCursor*>::iterator tobj;
-            list<ofxTuioCursor*> objectList = tuioclient.getTuioCursors();
-            for (tobj=objectList.begin(); tobj != objectList.end(); tobj++) {
-                ofxTuioCursor *blob = (*tobj);
-                if(blob->getSessionId() == RecordingBlobId){
-                    if(mRecorder.update(blob->getX()*640-10 , blob->getY()*480-10)==true) RecordingBlobId=-1;
-                    blobStillExists=true;
-                    break;
-                }
-            }
-            if(blobStillExists==false){
-                mRecorder.cancel();
-                RecordingBlobId=-1;
-                cout << "grabacion cancelada";
-            }
-        }
-    }
-//    mRecorder.update();
-    
+	tuioClient.getMessage();
+	float dt = 0.016666666;
+	sceneManager->update( dt );
+	
 }
 
-//--------------------------------------------------------------
+
 void testApp::draw(){
     
-    //
-        ofBackground(0);
-    
-    
-    if(appStatuses["mode"]==CAPTURE){
-         //ofDisableBlendMode();
-		ofSetColor(255);
-        mSyphonClient.draw(0, 0,640,480);
-        list<ofxTuioCursor*>::iterator tobj;
-        list<ofxTuioCursor*> objectList = tuioclient.getTuioCursors();
-        for (tobj=objectList.begin(); tobj != objectList.end(); tobj++) {
-            ofxTuioCursor *blob = (*tobj);
-            		ofSetColor(255,0,0);
-            ofEllipse( blob->getX()*640, blob->getY()*480,9,9);
-            //cout << "blob size" << blobTracker.trackedBlobs.size() << "\n";
-        }
-                    		ofSetColor(255);
-        fbo.begin();
-        ofClear(0, 0, 0, 0);
-        mSyphonClient2.draw(0,0,640,480);
-        //mSyphonClient.draw(0, 0,640,480);
-        fbo.end();
-        //fbo.draw(640,480,320,240);
-        fbo.readToPixels(remoteBlobImgPxl);
-        
-        feedImg.setFromPixels(remoteBlobImgPxl);
-        feedImg.update();
-        //feedImg.draw(640,480,320,240);
-        
-        //remoteBlobImgPxl=feedImg.getPixelsRef();
-        //cameraImg.setFromPixels(cameraPixels);
-        //cv::Mat imgMat = ofxCv::toCv(cameraPixels);
-        //mPlayer.drawAll(0,0);
-        
-        ofPushMatrix();
-        ofScale( 0.5, 1/1.5 );
-        //tuioclient.drawCursors();
-        ofPopMatrix();
-        
-        moveandrecord.draw();
-    }else  if(appStatuses["mode"]==MOSAIC){
-		ofPushMatrix();
-        mSyphonClient.draw(0,0,640,480);
-        ofSetColor(255,255,255);
+	sceneManager->draw();
+	ofPushStyle();
+	ofSetColor(255,0,0);
+	ofDrawBitmapString( "press 1, 2, or 3 to change scene", ofGetWidth() - 290, ofGetHeight() - 10);
+	ofPopStyle();
 
-        float tt = ofGetElapsedTimef();
-        
-        int ii=0;
-        /*for (std::vector<ofxImageSequence>::iterator it = sequences.begin() ;
-                                                it != sequences.end(); ++it){
-            int index= it->getCurrentFrame();
-			it->getFrame(index)->draw(points[ii][index].x,points[ii][index].y);//draw(RECTANGLESIZE*(ii%5),RECTANGLESIZE*floor(ii/5));
-            index++;
-            if(index> it->getTotalFrames() ) index=0;
-            it->setFrame(index);
-            ii++;
-            break;
-        } */
-        
-		//get the frame based on the current time and draw it
-         //ofDisableBlendMode();
-       // glDisable (GL_BLEND);
-        playImage(currentImg);
-        ofPopMatrix();
-    }
-  //
-	if(appStatuses["debug"]) showDebug();
 }
 
-void testApp::playImage(int i ){
-    int index = sequences[i].getCurrentFrame();
-    sequences[i].getFrame(index)->draw(points[i][index].x,points[i][index].y);
-    index++;
-    if(index> maxFrame/*sequences[i].getTotalFrames()*/ ) index=0;
-    sequences[i].setFrame(index);
+
+//if you require mouseMoved events in your scenes, forward them to the SceneManager. Same for any other events
+void testApp::mousePressed( int x, int y, int button ){
+	sceneManager->mousePressed(x, y, button);
 }
 
-void testApp::showDebug(){
-    ofDrawBitmapString("Framerate " + ofToString(ofGetFrameRate()), 20, 20);
-    if(appStatuses["mode"]==CAPTURE)
-    ofDrawBitmapString("mode: CAPTURE" , 20, 40);
-    else     ofDrawBitmapString("mode: DEBUG" , 20, 40);
-    // ofDrawBitmapString("NonZero " + ofToString(nonZero), 20, 40);
+void testApp::keyPressed(int key){
+	//if (key == '0') sceneManager->goToScene(SCENE_0);
+	if (key == '1') sceneManager->goToScene(SCENE_1, true); /* true >> regardless of curtain state (so u can change state while curtain is moving)*/
+	if (key == '2') sceneManager->goToScene(SCENE_2);
+	//if (key == '3') sceneManager->goToScene(SCENE_3);
+	//if (key == '4') sceneManager->goToScene(SCENE_4);
+    sceneManager->keyPressed(key);
 }
-/*
+
+
+void testApp::windowResized(int w, int h){
+	sceneManager->windowResized(w,h); // in case your screens need to know, will forward to all of them
+}
+
+
 void testApp::tuioAdded(ofxTuioCursor &tuioCursor){
-	ofPoint loc = ofPoint(tuioCursor.getX()*ofGetWidth(),tuioCursor.getY()*ofGetHeight());
-	//cout << "Point n" << tuioCursor.getSessionId() << " add at " << loc << endl;
+    cout << "tuio added";
+    sceneManager->getCurrentScene()->tuioAdded(tuioCursor);
 }
 
 void testApp::tuioUpdated(ofxTuioCursor &tuioCursor){
-	ofPoint loc = ofPoint(tuioCursor.getX()*ofGetWidth(),tuioCursor.getY()*ofGetHeight());
-	//cout << "Point n" << tuioCursor.getSessionId() << " updated at " << loc << endl;
+    sceneManager->getCurrentScene()->tuioUpdated(tuioCursor);
 }
 
 void testApp::tuioRemoved(ofxTuioCursor &tuioCursor){
-	ofPoint loc = ofPoint(tuioCursor.getX()*ofGetWidth(),tuioCursor.getY()*ofGetHeight());
-	//cout << "Point n" << tuioCursor.getSessionId() << " remove at " << loc << endl;
-}*/
-
-//--------------------------------------------------------------
-void testApp::keyPressed(int key){
-
-}
-void testApp::onRecordingFinished(int &num){
-    setupSequence(num);
-
+    sceneManager->getCurrentScene()->tuioRemoved(tuioCursor);
+    
 }
 
-//--------------------------------------------------------------
-void testApp::keyReleased(int key){
-    if(key=='r'){
-        mRecorder.start(3000, 25, remoteBlobImgPxl,
-                        moveandrecord.triggerRectangle.width,moveandrecord.triggerRectangle.height,
-                        moveandrecord.triggerRectangle.x,moveandrecord.triggerRectangle.y);
-    }
-    else if(key=='m'){
-        if(appStatuses["mode"]==CAPTURE)
-            appStatuses["mode"]=MOSAIC;
-        else appStatuses["mode"]=CAPTURE;
-    }
 
-}
 
-//--------------------------------------------------------------
-void testApp::mouseMoved(int x, int y ){
-
-}
-
-//--------------------------------------------------------------
-void testApp::mouseDragged(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void testApp::mousePressed(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void testApp::mouseReleased(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void testApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void testApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void testApp::dragEvent(ofDragInfo dragInfo){ 
-
-}
-
-void testApp::gui2Event(ofxUIEventArgs &e)
-{
-    cout << "event";
-	string name = e.widget->getName();
-	int kind = e.widget->getKind();
-    cout << "name" << name << "\n";
-    cout << "kind" << kind << "\n";
-    if(kind==2){
-        if(name == "2")
-        {
-            currentImg=2;
-        }
-        if(name == "1")
-        {
-            currentImg=1;
-        }
-        if(name == "0")
-        {
-            currentImg=0;
-        }
-    }
-    if(kind==OFX_UI_WIDGET_TOGGLE && name=="grabacion"){
-        if(appStatuses["mode"]==CAPTURE)
-            appStatuses["mode"]=MOSAIC;
-        else appStatuses["mode"]=CAPTURE;
-    }
-}
